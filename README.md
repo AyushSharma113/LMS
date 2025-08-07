d648e6a83928bee83017ee0cd8f8270bdf2b8405
AyushSharma113 <techtinkerzone9@gmail.com>
2025-08-07 21:18:06 +0530

    login and signup functionalities

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ create mongodb cluster
 set up mongoose
 dotenv setup
 now start creating schema and controller
+
+F5p5jkPPUaspEras
+mongodb+srv://hacker99034:F5p5jkPPUaspEras@lmssystem0.z85nwxa.mongodb.net/
--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
 import connectDB from "./database/db.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 dotenv.config({});
 
 connectDB();
 const app = express();
+
+// default middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/v1/user", userRoute);
+
+// USER_API = http://localhost:5173/api/v1/user;
 
 // server starting
 const PORT = process.env.PORT || 3000;
