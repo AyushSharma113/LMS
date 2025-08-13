d156c6d3aef68bb04947aed59048aee96c248d53
AyushSharma113 <techtinkerzone9@gmail.com>
2025-08-13 07:20:46 +0530

    added navbar and dark mode

--- lmssystem/src/App.jsx
+++ lmssystem/src/App.jsx
@@ -1,11 +1,13 @@
 // import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 import { Button } from "./components/ui/button";
 import TabsDemo from "./pages/Login";
 
 function App() {
   return (
     <>
+      <Navbar />
       <TabsDemo />
     </>
   );
--- lmssystem/src/main.jsx
+++ lmssystem/src/main.jsx
@@ -5,12 +5,15 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import { Toaster } from "./components/ui/sonner";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
-      <Toaster />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+        <Toaster />
+      </ThemeProvider>
     </Provider>
   </StrictMode>
 );
