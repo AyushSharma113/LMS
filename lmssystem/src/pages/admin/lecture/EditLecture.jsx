667b3dcb5d684466db0da8a705515b88d9a02898
AyushSharma113 <techtinkerzone9@gmail.com>
2025-08-27 20:27:43 +0530

    added edit lecture page

--- lmssystem/src/pages/admin/lecture/EditLecture.jsx
+++ lmssystem/src/pages/admin/lecture/EditLecture.jsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
 import React from "react";
 import { Link, useParams } from "react-router-dom";
+import LectureTab from "./LectureTab";
 // import LectureTab from "./LectureTab";
 
 const EditLecture = () => {
@@ -19,7 +20,7 @@ const EditLecture = () => {
           <h1 className="font-bold text-xl">Update Your Lecture</h1>
         </div>
       </div>
-      {/* <LectureTab /> */}
+      <LectureTab />
     </div>
   );
 };
