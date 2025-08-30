5c42e4a4e5742d98d3ab0c7cbcb544f65b158892
Ayush Sharma <sharmaayushkumar15@gmail.com>
2025-08-30 21:12:58 +0530

    define functionality for video uploading

--- lmssystem/src/pages/admin/lecture/LectureTab.jsx
+++ lmssystem/src/pages/admin/lecture/LectureTab.jsx
@@ -10,11 +10,13 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Progress } from "@/components/ui/progress";
 import { Switch } from "@/components/ui/switch";
+import axios from "axios";
 import { Loader2 } from "lucide-react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { toast } from "sonner";
 
-const MEDIA_API = "http://localhost:8080/api/v1/media";
+const MEDIA_API = "http://localhost:8000/api/v1/media";
 
 const LectureTab = () => {
   const removeLoading = false;
@@ -25,8 +27,40 @@ const LectureTab = () => {
   const [uploadProgress, setUploadProgress] = useState(0);
   const [btnDisable, setBtnDisable] = useState(true);
   const params = useParams();
-  const { courseId, lectureId } = params;
+  // const { courseId, lectureId } = params;
   const isLoading = false;
+
+  const fileChangeHandler = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+      setMediaProgress(true);
+      try {
+        const res = await axios.post(`${MEDIA_API}/upload-video`, formData, {
+          onUploadProgress: ({ loaded, total }) => {
+            setUploadProgress(Math.round((loaded * 100) / total));
+          },
+        });
+
+        if (res.data.success) {
+          console.log(res);
+          setUploadVideoInfo({
+            videoUrl: res.data.data.url,
+            publicId: res.data.data.public_id,
+          });
+          setBtnDisable(false);
+          toast.success(res.data.message);
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error("video upload failed");
+      } finally {
+        setMediaProgress(false);
+      }
+    }
+  };
+
 
   const editLectureHandler = () => {
     console.log("what the fuck is this");
@@ -75,7 +109,7 @@ const LectureTab = () => {
           <Input
             type="file"
             accept="video/*"
-            // onChange={fileChangeHandler}
+            onChange={fileChangeHandler}
             placeholder="Ex. Introduction to Javascript"
             className="w-fit"
           />
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import connectDB from "./database/db.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import courseRoute from "./routes/course.route.js";
+import mediaRoute from "./routes/media.route.js"
 dotenv.config({});
 
 connectDB();
@@ -20,6 +21,7 @@ app.use(
   })
 );
 
+app.use("/api/v1/media", mediaRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/course", courseRoute);
 
